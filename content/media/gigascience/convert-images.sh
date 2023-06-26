4f454e5563f409706678ffdcfe0da43fe4dcdd9c
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2023-06-26 14:11:09 -0400

    giad047_f5.eps: prevent rasterization
    https://github.com/greenelab/connectivity-search-manuscript/issues/53#issuecomment-1607977358

--- content/media/gigascience/convert-images.sh
+++ content/media/gigascience/convert-images.sh
@@ -16,9 +16,9 @@ cp content/media/rephetio/metagraph-and-features.eps content/media/gigascience/g
 curl --location --silent https://github.com/greenelab/connectivity-search-analyses/raw/15f1925c0481d8e6bab8b0931f48f2fad388c68c/explore/degree-group-analyses.pdf > content/media/gigascience/giad047_f4.pdf
 pdftops content/media/gigascience/giad047_f4.pdf content/media/gigascience/giad047_f4.eps
 
-# Figure 5
+# Figure 5: warning legend has transparency that is not supported  by encapsulated postscript
 curl --location --silent https://github.com/greenelab/connectivity-search-analyses/raw/1c6827ce2544c17cef42bbccf098a312f2c44f97/explore/gamma-hurdle/gamma-hurdle-distributions.pdf > content/media/gigascience/giad047_f5.pdf
-pdftops content/media/gigascience/giad047_f5.pdf content/media/gigascience/giad047_f5.eps
+pdftops -rasterize never content/media/gigascience/giad047_f5.pdf content/media/gigascience/giad047_f5.eps
 
 # Figure 6
 curl --location --silent https://github.com/greenelab/connectivity-search-backend/raw/752b423a4b7b57575d66ce0b797b0a84c23267a6/media/models-schema.pdf > content/media/gigascience/giad047_f6.pdf
